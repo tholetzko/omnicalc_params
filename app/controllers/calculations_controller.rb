c0494fe554c3922c0935ce24bf3c3879c8885eb4
Tobias <tholetzko@users.noreply.github.com>
2017-04-20 00:40:34 -0500

    Flexible Payments

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -15,14 +15,25 @@ class CalculationsController < ApplicationController
     render("square_root.html.erb")
   end
 
-# Present value
-def present_value
-  @number_to_square_root = params[:number].to_i
-  @square_root = Math.sqrt(@number_to_square_root).round(2)
-  render("square_root.html.erb")
-end
 
-# MinMax
+# Present value
+def loan_payment
+    @apr = params[:basis_points].to_f / 100
+    @years = params[:number_of_years].to_i
+    @principal = params[:present_value].to_i
+
+    r = @apr.to_f / 100 / 12
+    p = @principal.to_f
+    n = @years * 12
+    @monthly_payment = ( r / ( 1 - (1 + r) ** (-1 * n))) * p
+    @monthly_payment_output = @monthly_payment.round(2)
+
+    # ================================================================================
+    # Your code goes above.
+    # ================================================================================
+
+    render("loan_payment.html.erb")
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ get("/flexible/square/:number",                 { :controller => "calculations",
 get("/flexible/square_root/:number",                 { :controller => "calculations", :action => "square_root" })
 
 # Present value
-get("/flexible/payment/:basis_points/:number_of_years/:present_value",                 { :controller => "calculations", :action => "present_value" })
+get("/flexible/payment/:basis_points/:number_of_years/:present_value",                 { :controller => "calculations", :action => "loan_payment" })
 
 # MinMax
-get("/flexible/random/:min/:max",                 { :controller => "calculations", :action => "min_max" })
+# get("/flexible/random/:min/:max",                 { :controller => "calculations", :action => "min_max" })
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
